2422614938e9028377902efc9553f057609686ed
anomalilies <takitafu8@gmail.com>
2021-03-16 06:26:18 +0000

    Work in Progress

--- index.ts
+++ index.ts
@@ -6,14 +6,28 @@ const client = new Discord.Client();
 import map from "./data/map.json";
 
 client.once("ready", () => {
-  console.log("Ready!");
-
   const timeout = (ms: number) => new Promise((res) => setTimeout(res, ms));
+  const filter = (m: { author: { id: string; }; }) => m.author.id === "500385855072894982";
+
   async function loopThroughMembers() {
+    const channel = await client.channels.fetch("747201864889794721");
+
     for (var i = 0; i < map.length; i++) {
-      await client.channels
-        .fetch("747201864889794721")
-        .then(async (channel: any) => await channel.send(`-fs ${map[i]}`));
+      await channel.send(`-fs ${map[i]}`);
+
+      channel.awaitMessages(filter, { max: 1 }).then((m: any) => {
+        var desc = m.embeds[0].description.replace(
+          /([^\d\n]+)|([^\n].*$)/g,
+          ""
+        );
+        var fields = m.embeds[0].fields[0].value
+          .match(/(?<=\w*:\s\*\*)(.*?)(?=\*)/g)
+          .join("\n");
+
+        var info = `${desc.toString()}${fields.toString()}`;
+        console.log(map[i], info);
+      });
+
       await timeout(8000);
     }
   }
